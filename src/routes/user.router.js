a8257476881e3541c7e8a55aca0b0b8ef7d0f1a2
Ardhika <ardhikaptr11@gmail.com>
2025-04-29 11:03:01 +0700

    feat: add update and delete routes for user management

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -2,12 +2,17 @@ const express = require("express");
 
 const router = express.Router();
 
-const { createNewUser, getUsers, getOneUser } = require("../controllers/user.controller");
+const { createNewUser, getUsers, getOneUser, updateUserData, deleteUserData } = require("../controllers/user.controller");
 
+// Create new user
 router.post("/user", createNewUser);
 // Get all users
 router.get("/users", getUsers);
 // Get one user by ID
-router.get("/users/:id", getOneUser);
+router.get("/user/:id", getOneUser);
+// Update user
+router.patch("/user/update/:id", updateUserData);
+// Delete user
+router.delete("/user/:id", deleteUserData);
 
 module.exports = router;
