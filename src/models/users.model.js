6d9799cad354f6447f5180cbd65ce06c18177dc5
Ardhika <ardhikaptr11@gmail.com>
2025-04-29 10:59:58 +0700

    feat: update user model for complete CRUD operations

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -3,15 +3,9 @@ const bcrypt = require("bcryptjs");
 
 const TABLE_NAME = "users";
 
-const create = async (data) => {
+const createUser = async (data) => {
 	try {
 		const { fullName, email, gender, phone, password } = data;
-
-		const isUserExist = await knex(TABLE_NAME).where({ email }).first();
-
-        if (isUserExist) {
-
-        }
 
 		const salt = await bcrypt.genSalt(10);
 		const hashedPassword = await bcrypt.hash(password, salt);
@@ -33,7 +27,7 @@ const create = async (data) => {
 	}
 };
 
-const getAll = async () => {
+const getAllUsers = async () => {
 	return knex(TABLE_NAME).select("user_id", "fullName", "email", "gender", "phone").orderBy("user_id", "asc");
 };
 
@@ -42,13 +36,28 @@ const getUserById = async (userId) => {
 };
 
 const getUserByEmail = async (email) => {
-    return knex(TABLE_NAME).where({ email }).first();
+	return knex(TABLE_NAME).where({ email }).first();
 };
 
+const updateUser = async (userId, data) => {
+	const result = await knex(TABLE_NAME)
+		.update(data)
+		.where("user_id", userId)
+		.returning(["user_id", "fullName", "email", "phone"]);
+
+	return result;
+};
+
+const deleteUser = async (userId) => {
+	return knex(TABLE_NAME).where("user_id", userId).del();
+}
+
 module.exports = {
-	create,
-	getAll,
+	createUser,
+	getAllUsers,
 	getUserById,
-    getUserByEmail,
+	getUserByEmail,
+	updateUser,
+	deleteUser,
 	tableName: TABLE_NAME
 };
