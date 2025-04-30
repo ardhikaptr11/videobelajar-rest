2fb09024b6210baa2902b87624d29d77ed9cedbc
Ardhika <ardhikaptr11@gmail.com>
2025-05-01 00:53:09 +0700

    feat: add course router to the main routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const routes = [require("./user.router")];
+const routes = [require("./user.router"), require("./course.router")];
 
 routes.forEach((route) => {
 	router.use("/api/v1", route);
