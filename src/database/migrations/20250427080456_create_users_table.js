b3a69ceed911cceff4139ff9c10df249fa554b44
Ardhika <ardhikaptr11@gmail.com>
2025-05-01 00:52:18 +0700

    refactor: change user migration function to arrow function syntax

--- src/database/migrations/20250427080456_create_users_table.js
+++ src/database/migrations/20250427080456_create_users_table.js
@@ -2,7 +2,7 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function (knex) {
+exports.up = (knex) => {
 	return knex.schema.createTable("users", (table) => {
 		table.increments("user_id").primary();
 		table.string("fullName", 100).notNullable();
@@ -23,7 +23,7 @@ exports.up = function (knex) {
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = async function (knex) {
+exports.down = async (knex) => {
 	await knex.schema.dropTableIfExists("users");
 	await knex.raw("DROP TYPE IF EXISTS gender_enum");
 };
