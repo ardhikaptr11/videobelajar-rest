464f4e80a9aaad541b559c118d988c455add0cb4
Ardhika <ardhikaptr11@gmail.com>
2025-05-05 11:59:49 +0700

    refactor: simplify course creation logic and enhance module handling

--- src/models/courses.model.js
+++ src/models/courses.model.js
@@ -4,49 +4,31 @@ const knex = require("./knex");
 
 const TABLE_NAME = "courses";
 
-const createCourse = async (data) => {
-	const { name, tagline, description, total_modules, price, thumbnail_img_url, categories } = data;
+const createCourse = (trx, data) => {
+	return trx(TABLE_NAME).insert(data).returning(["course_id", "name", "created_at"]);
+};
 
-	const slug = faker.helpers.slugify(name.replace(/\/| - |-/g, " ").toLocaleLowerCase());
+const createCategory = async (trx, name) => {
+	const [category] = await trx("categories").insert({ name }).returning("category_id");
 
-	const categoriesMappedIds = await Promise.all(
-		categories.map(async (name) => {
-			const categoryId = await knex("categories").select("category_id").where({ name }).first();
+	const categoryId = category.category_id;
+	return categoryId;
+};
 
-			if (!categoryId) {
-				const [newCategory] = await knex("categories").insert({ name }).returning("category_id");
-
-				return newCategory.category_id;
-			}
-
-			return categoryId.category_id;
-		})
-	);
-
-	const [course] = await knex(TABLE_NAME)
-		.insert({
-			name,
-			tagline,
-			description,
-			slug,
-			total_modules,
-			price,
-			thumbnail_img_url
-		})
-		.returning(["course_id", "name"]);
-
-	course.total_students_enrolled = 0;
-	course.categories = categories;
-	course.created_at = new Date();
-
-	const courseCategories = categoriesMappedIds.map((categoryId) => ({
-		course_id: course.course_id,
-		category_id: categoryId
+// modules:
+// [{"title": "Module 1"}, {"name": "Module 2"}, {"name": "Module 3"}]
+const createCourseModules = (trx, courseId, modules) => {
+	const dataToInsert = modules.map((module) => ({
+		course_id: courseId,
+		title: module.title
 	}));
 
-	await Promise.all(courseCategories.map((category) => knex("course_categories").insert(category)));
+	return trx("modules").insert(dataToInsert);
+};
 
-	return course;
+const createCourseCategoryRelation = (trx, courseId, categories) => {
+	const dataToInsert = categories.map((categoryId) => ({ course_id: courseId, category_id: categoryId }));
+	return trx("course_categories").insert(dataToInsert);
 };
 
 const getAllCourses = async () => {
@@ -57,13 +39,24 @@ const getAllCourses = async () => {
 			"c.tagline",
 			"c.description",
 			"c.slug",
-			"c.total_modules",
 			"c.price",
 			"c.discounted_price",
 			"c.is_discount",
 			"c.thumbnail_img_url",
-			knex.raw("count(uc.user_id) total_students_enrolled"),
-			knex.raw("array_agg(distinct cat.name) categories")
+			knex.raw("count(uc.user_id)::int as total_students_enrolled"),
+			knex.raw("array_agg(distinct cat.name) as categories"),
+			knex.raw(
+				`
+					(
+						SELECT jsonb_agg(modules_row ORDER BY (modules_row->>'module_id')::int)
+						FROM (
+							SELECT DISTINCT jsonb_build_object('module_id', m.module_id, 'title', m.title) as modules_row
+							FROM modules m
+							WHERE m.course_id = c.course_id
+						) as sub
+					) as modules
+				`
+			)
 		)
 		.leftJoin({ uc: "user_course" }, "c.course_id", "uc.course_id")
 		.leftJoin({ cc: "course_categories" }, "c.course_id", "cc.course_id")
@@ -80,113 +73,76 @@ const getCourseById = async (courseId) => {
 			"c.tagline",
 			"c.description",
 			"c.slug",
-			"c.total_modules",
 			"c.price",
 			"c.discounted_price",
 			"c.is_discount",
 			"c.thumbnail_img_url",
-			knex.raw("count(uc.user_id) total_students_enrolled"),
-			knex.raw("array_agg(distinct cat.name) categories")
+			knex.raw("count(uc.user_id) as total_students_enrolled"),
+			knex.raw("array_agg(distinct cat.name) as categories"),
+			knex.raw(
+				`
+					(
+						SELECT jsonb_agg(modules_row ORDER BY (modules_row->>'module_id')::int)
+						FROM (
+							SELECT DISTINCT jsonb_build_object('module_id', m.module_id, 'title', m.title) as modules_row
+							FROM modules m
+							WHERE m.course_id = c.course_id
+						) as sub
+					) as modules
+				`
+			)
 		)
 		.leftJoin({ uc: "user_course" }, "c.course_id", "uc.course_id")
 		.leftJoin({ cc: "course_categories" }, "c.course_id", "cc.course_id")
 		.leftJoin({ cat: "categories" }, "cc.category_id", "cat.category_id")
-		.groupBy("c.course_id")
 		.where("c.course_id", courseId)
+		.groupBy("c.course_id")
 		.first();
 };
 
 const getCourseByName = async (name) => {
-	return knex(TABLE_NAME).where({ name }).first();
+	return knex(TABLE_NAME).select("course_id").where({ name }).first();
 };
 
-const updateCourse = async (courseId, data, paramValue) => {
-	if (data.name) {
-		const slug = faker.helpers.slugify(data.name.replace(/\/| - |-/g, " ").toLowerCase());
-		data.slug = slug;
-	}
+const getCourseCategories = async (trx, courseId) => {
+	return trx("course_categories").select("category_id").where({ course_id: courseId });
+};
 
-	if (data.categories && paramValue === "replace") {
-		const categoryNames = data.categories;
+const getCategoryIdByName = async (trx, name) => {
+	const category = await trx("categories").select("category_id").where({ name }).first();
 
-		const categories = await knex("categories").select("category_id").whereIn("name", categoryNames);
-		const categoriesMappedIds = categories.map((category) => category.category_id);
+	if (!category) return null;
 
-		const currentCategories = await knex("course_categories").select("category_id").where({ course_id: courseId });
-		const currentCategoryIds = currentCategories.map((c) => c.category_id);
+	const categoryId = category.category_id;
 
-		await knex.transaction(async (trx) => {
-			await trx("course_categories")
-				.where({ course_id: courseId })
-				.whereIn("category_id", currentCategoryIds)
-				.del();
+	return categoryId;
+};
 
-			await trx("course_categories").insert(
-				categoriesMappedIds.map((categoryId) => ({
-					course_id: courseId,
-					category_id: categoryId
-				}))
-			);
-		});
-	}
-
-	delete data.categories;
-	await knex(TABLE_NAME).update(data).where("course_id", courseId);
-
-	const updatedCourse = await getCourseById(courseId);
-	return updatedCourse;
+const updateCourse = async (trx, courseId, data) => {
+	return trx(TABLE_NAME).update(data).where("course_id", courseId);
 };
 
 const deleteCourse = async (courseId) => {
-	const currentCategories = await knex("course_categories").select("category_id").where({ course_id: courseId });
-	const currentCategoryIds = currentCategories.map((c) => c.category_id);
-
-	await knex.transaction(async (trx) => {
-		await trx("course_categories").where({ course_id: courseId }).whereIn("category_id", currentCategoryIds).del();
-	});
-
 	return knex(TABLE_NAME).where("course_id", courseId).del();
 };
 
-// const categoriesToInsert = categoriesMappedIds.filter((categoryId) => !currentCategoryIds.includes(categoryId));
-// const currentCategories = await knex("course_categories").select("category_id").where({ course_id: courseId });
+const deleteCategory = (trx, courseId, categoryIds) => {
+	return trx("course_categories").where({ course_id: courseId }).whereIn("category_id", categoryIds).del();
+};
 
-// const currentCategoryIds = currentCategories.map((category) => category.category_id);
-
-// const anyMatchingCategory = categoriesMappedIds.some((categoryId) => currentCategoryIds.includes(categoryId));
-
-// if (anyMatchingCategory) {
-// 	return;
-// }
-
-// data = {
-// 	name: "Course Name",
-// 	tagline: "Course Tagline",
-// 	description: "Course Description",
-// 	total_modules: 10,
-// 	price: 399,
-// 	thumbnail_img_url: "https://example.com/image.jpg",
-// 	categories: ["Web Development", "Security Development", "Internet Technologies"]
-// };
-
-// updateCourse(1, data, "replace");
-
-// data = {
-// 	"name": "Course Name",
-// 	"tagline": "Course Tagline",
-// 	"description": "Course Description",
-// 	"total_modules": 10,
-// 	"price": 399,
-// 	"thumbnail_img_url": "https://example.com/image.jpg",
-// 	"categories": ["Category 1", "Category 2"]
-// }
 
 module.exports = {
 	createCourse,
+	createCategory,
+	createCourseModules,
+	createCourseCategoryRelation,
 	getAllCourses,
 	getCourseById,
 	getCourseByName,
+	getCourseCategories,
+	getCategoryIdByName,
 	updateCourse,
 	deleteCourse,
+	deleteCategory,
 	tableName: TABLE_NAME
 };
