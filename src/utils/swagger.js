7a1fa5639a824387b1cc1326b1b0a58b342a5cc1
Ardhika <ardhikaptr11@gmail.com>
2025-05-05 12:00:32 +0700

    feat: add Swagger documentation setup for API

---
+++ src/utils/swagger.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs")
+const path = require("path");
+const swaggerDocument = YAML.load(path.join(__dirname, "../../docs.yaml"));
+
+const swaggerDocs = (app, port) => {
+	app.use("/docs/api/v1", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+	console.log(`Docs are available at http://localhost:${port}/docs/api/v1`);
+};
+
+module.exports = swaggerDocs;
