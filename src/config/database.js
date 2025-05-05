8a8ce722bb46d3b6e478237f675cf41f0b3f07c5
Ardhika <ardhikaptr11@gmail.com>
2025-05-05 18:08:07 +0700

    fix: update database configuration to use correct environment variable names

--- src/config/database.js
+++ src/config/database.js
@@ -6,11 +6,11 @@ const connection =
 	process.env.NODE_ENV === "development"
 		? process.env.PGURL
 		: {
-				user: process.env.PGUSER,
-				password: process.env.PGPASSWORD,
-				host: process.env.PGHOST,
-				port: process.env.SERVER_PORT,
-				database: process.env.PGDATABASE
+				user: process.env.POSTGRES_USER,
+				password: process.env.POSTGRES_PASSWORD,
+				host: process.env.POSTGRES_HOST,
+				port: process.env.POSTGRES_PORT,
+				database: process.env.POSTGRES_DATABASE
 		  };
 
 const common = {
