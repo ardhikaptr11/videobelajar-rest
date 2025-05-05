39388f4384e9679e61273ea5376581e358a529c7
Ardhika <ardhikaptr11@gmail.com>
2025-05-05 11:59:07 +0700

    feat: update server response message and integrate Swagger documentation

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ require("@dotenvx/dotenvx").config({ path: path.join(__dirname, "../.env") });
 
 const express = require("express");
 const routes = require("./routes");
+const swaggerDocs = require("./utils/swagger");
 
 const app = express();
 
@@ -12,21 +13,23 @@ app.use(express.json());
 
 app.use(routes);
 
+const SERVER_PORT = process.env.SERVER_PORT || 8765;
+
 /**
  * @type { import("express").Request } _req
  * @type { import("express").Response } res
  * @type { import("express").NextFunction } _next
  */
 app.get("/api/v1", (_req, res, _next) => {
-    res.send({
-        serverMessage: "Server is running.",
-        serverTime: new Date().toLocaleString()
-    });
+	res.send({
+		serverMessage: `Server is up and running. Docs are available at http://localhost:${SERVER_PORT}/docs/api/v1`,
+		serverTime: new Date().toLocaleString()
+	});
 });
-
-const SERVER_PORT = process.env.SERVER_PORT || 8765;
 
 app.listen(process.env.SERVER_PORT || 8765, () => {
 	console.clear();
 	console.log(`Server is running on port ${SERVER_PORT}.`);
+
+	swaggerDocs(app, SERVER_PORT);
 });
