bb571027c8b844e14806dc80084a831b00abba6a
Ardhika <ardhikaptr11@gmail.com>
2025-05-05 13:48:42 +0700

    refactor: restructure database configuration for clarity and consistency

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,25 @@ const path = require("path");
 
 require("@dotenvx/dotenvx").config({ path: path.join(__dirname, "../../.env") });
 
-const connection = process.env.PGURL;
+const connection =
+	process.env.NODE_ENV === "development"
+		? process.env.PGURL
+		: {
+				user: process.env.PGUSER,
+				password: process.env.PGPASSWORD,
+				host: process.env.PGHOST,
+				port: process.env.SERVER_PORT,
+				database: process.env.PGDATABASE
+		  };
+
+const common = {
+	client: "pg",
+	connection,
+	migrations: {
+		directory: path.join(__dirname, "../database/migrations"),
+		tableName: "migrations"
+	}
+};
 
 /**
  * @type { Object.<string, import("knex").Knex.Config }
@@ -10,14 +28,13 @@ const connection = process.env.PGURL;
 
 module.exports = {
 	development: {
-		client: "pg",
-		connection,
-		migrations: {
-			directory: path.join(__dirname, "../database/migrations"),
-			tableName: "migrations"
-		},
+		...common,
+		connection: process.env.PGURL,
 		seeds: {
 			directory: path.join(__dirname, "../database/seeds")
-		},
+		}
+	},
+	production: {
+		...common
 	}
 };
