f25517e3500b98feec402ac5674c882f92a90d9f
Ardhika <ardhikaptr11@gmail.com>
2025-05-08 14:38:42 +0700

    fix: update environment variable loading for different NODE_ENV configurations

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,16 @@
 const path = require("path");
 
-require("@dotenvx/dotenvx").config({ path: path.join(__dirname, "../../.env") });
+if (process.env.NODE_ENV !== "production") {
+	const envFile = process.env.NODE_ENV === "development" ? ".env" : `.env.${process.env.NODE_ENV}`;
+	require("@dotenvx/dotenvx").config({ path: path.join(__dirname, "../../", envFile) });
+}
 
-const connection = process.env.NODE_ENV === "development" ? process.env.POSTGRES_URL_DEV : process.env.POSTGRES_URL;
+const connection =
+	process.env.NODE_ENV === "development"
+		? process.env.POSTGRES_URL_DEV
+		: process.env.NODE_ENV === "test"
+		? process.env.POSTGRES_URL_TEST
+		: process.env.POSTGRES_URL;
 
 const common = {
 	client: "pg",
@@ -25,5 +33,11 @@ module.exports = {
 	},
 	production: {
 		...common
+	},
+	test: {
+		...common,
+		seeds: {
+			directory: path.join(__dirname, "../database/seeds")
+		}
 	}
 };
