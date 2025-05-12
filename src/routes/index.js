c89fcf0b2d5ef1554a3217cfe04d82e07e98e4f3
Ardhika <ardhikaptr11@gmail.com>
2025-05-12 13:21:43 +0700

    feat: update API version to v2 and include auth router

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,10 @@ const express = require("express");
 
 const router = express.Router();
 
-const routes = [require("./user.router"), require("./course.router")];
+const routes = [require("./user.router"), require("./course.router"), require("./auth.router")];
 
 routes.forEach((route) => {
-	router.use("/api/v1", route);
+	router.use("/api/v2", route);
 });
 
 module.exports = router;
