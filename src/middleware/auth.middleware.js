cb05513917d1d51608c0e8c80bc6856400ba6849
Ardhika <ardhikaptr11@gmail.com>
2025-05-13 13:44:43 +0700

    fix: correct authorization header casing and streamline token validation logic

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -16,7 +16,11 @@ require("@dotenvx/dotenvx").config({
  */
 const verifyToken = async (req, res, next) => {
 	try {
-		const authHeader = req.get("authorization");
+		const authHeader = req.get("Authorization"); // Bearer ...
+		// console.log(authHeader);
+
+		if (!authHeader)
+			return res.status(401).json({ code: 401, message: "Authentication failed, no token provided" });
 
 		if (authHeader.split(" ").length !== 2) {
 			return res.status(401).json({
@@ -25,13 +29,12 @@ const verifyToken = async (req, res, next) => {
 				data: null
 			});
 		}
-
-		const token = authHeader.split(" ")[1];
-
-		if (!token) return res.status(401).json({ code: 401, message: "Authentication failed, no token provided" });
-
+		
+		const token = authHeader.split(" ")[1]
 		const decoded = jwt.verify(token, process.env.JWT_SECRET, { issuer: "videobelajar-api" });
 		req.user = decoded;
+
+		if (decoded.role !== "admin") return res.status(403).json({ message: "Forbidden: You are not authorized" });
 
 		next();
 	} catch (error) {
