919b4b62a3ca949a40a1bf82f026df89b2c485a9
Ardhika <ardhikaptr11@gmail.com>
2025-05-14 11:35:49 +0700

    refactor: update user verification token migration to use async/await

--- src/database/migrations/20250513143028_change_verif_token_column_schema.js
+++ src/database/migrations/20250513143028_change_verif_token_column_schema.js
@@ -2,10 +2,12 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = (knex) => {
-	return knex.schema.alterTable("users", (table) => {
+exports.up = async (knex) => {
+	await knex.schema.alterTable("users", (table) => {
 		table.string("verif_token", 8).notNullable().alter();
 	});
+
+	await knex("users").where("is_verified", false).update({ is_verified: true });
 };
 
 /**
