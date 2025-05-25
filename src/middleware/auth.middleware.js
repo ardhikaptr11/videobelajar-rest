e34e9b2f8808e47d53c1209ed62124b5ee7dc7c9
Ardhika <ardhikaptr11@gmail.com>
2025-05-25 11:26:56 +0700

    refactor: improve readability to authentication middleware for token validation and error handling

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,6 +1,9 @@
 const jwt = require("jsonwebtoken");
 const path = require("path");
 
+const knex = require("../models/knex");
+const throwError = require("../services/errorHandling.service");
+
 require("@dotenvx/dotenvx").config({
 	path: path.join(
 		__dirname,
@@ -14,37 +17,24 @@ require("@dotenvx/dotenvx").config({
  * @param { import("express").Response } res
  * @param { import("express").NextFunction } next
  */
-const verifyToken = async (req, res, next) => {
+const authenticate = async (req, res, next) => {
 	try {
 		const authHeader = req.get("Authorization");
 
-		if (!authHeader)
-			return res.status(401).json({ code: 401, message: "Authentication failed, no token provided" });
+		if (!authHeader || authHeader.split(" ").length !== 2)
+			throwError("Authentication failed. Invalid or missing token", 401);
 
-		if (authHeader.split(" ").length !== 2) {
-			return res.status(401).json({
-				code: 401,
-				message: "Invalid token",
-				data: null
-			});
-		}
-		
-		const token = authHeader.split(" ")[1]
+		const token = authHeader.split(" ")[1];
+
 		const decoded = jwt.verify(token, process.env.JWT_SECRET, { issuer: "videobelajar-api" });
 		req.user = decoded;
 
-		if (decoded.role !== "admin") return res.status(403).json({ message: "Forbidden: You are not authorized" });
-
 		next();
 	} catch (error) {
-		if (error.message === "JsonWebTokenError") {
-			return res.status(401).json({
-				code: 401,
-				message: error.message,
-				data: null
-			});
-		}
+		if (error.name === "JsonWebTokenError") throwError(error.message, 401);
+
+		return next(error);
 	}
 };
 
-module.exports = verifyToken;
+module.exports = authenticate;
