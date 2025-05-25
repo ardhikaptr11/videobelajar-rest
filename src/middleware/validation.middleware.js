ebfff19ec7cb49717913b6859ce5d23e2ee67f35
Ardhika <ardhikaptr11@gmail.com>
2025-05-25 11:32:04 +0700

    feat: enhance validation middleware with improved error handling and user data checks

--- src/middleware/validation.middleware.js
+++ src/middleware/validation.middleware.js
@@ -1,6 +1,9 @@
 const bcrypt = require("bcryptjs");
 
+const { accessUserDataByRole } = require("../services/user.service");
 const { getUserByEmail, getUserByVerifToken } = require("../models/users.model");
+const { getCourseById, getCourseByName } = require("../models/courses.model");
+const throwError = require("../services/errorHandling.service");
 
 const emailPattern = /^[a-zA-Z][a-zA-Z0-9-_.]*@(?:gmail\.com|(?!gmail\.)[a-z]+\.(?:com|co\.[a-z]{2,3}))$/;
 
@@ -11,35 +14,37 @@ const emailPattern = /^[a-zA-Z][a-zA-Z0-9-_.]*@(?:gmail\.com|(?!gmail\.)[a-z]+\.
  * @param { import("express").NextFunction } next
  */
 const validateRegister = async (req, res, next) => {
-	const { full_name, email, gender, phone, password } = req.body;
-	const isPayloadEmpty = Object.keys(req.body).length === 0;
+	try {
+		const { full_name, email, gender, phone, password } = req.body;
+		const isPayloadEmpty = Object.keys(req.body).length === 0;
 
-	const passwordPattern = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/;
-	const phonePattern = /^08[1-9][0-9]{7,10}$/;
+		const passwordPattern = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/;
+		const phonePattern = /^8[1-9][0-9]{8,11}$/;
 
-	if (!full_name || !email || !gender || !phone || !password || isPayloadEmpty)
-		return res.status(422).json({ code: 422, message: "Please make sure all fields are filled in", data: null });
+		if (email.includes("admin")) throwError("Unable to use reserved email", 200);
 
-	if (email.includes("admin"))
-		return res.status(200).json({ code: 200, message: "Unable to use reserved email", data: null });
+		const isEmailAllowed = emailPattern.test(email);
+		if (!isEmailAllowed) throwError("Invalid email", 200);
 
-	const isUserExist = await getUserByEmail(email);
+		const isPhoneAllowed = phonePattern.test(phone);
+		if (!isPhoneAllowed) throwError("Invalid phone number", 200);
 
-	if (isUserExist) return res.status(400).json({ code: 400, message: "Email already registered", data: null });
+		const isPasswordAllowed = passwordPattern.test(password);
+		if (!isPasswordAllowed) throwError("Password is too weak", 200);
 
-	if (!["male", "female"].includes(gender))
-		return res.status(400).json({ code: 400, message: "Unacceptable gender value", data: null });
+		if (!full_name || !email || !gender || !phone || !password || isPayloadEmpty)
+			throwError("Please make sure all fields are filled in", 400);
 
-	const isEmailAllowed = emailPattern.test(email);
-	if (!isEmailAllowed) return res.status(200).json({ code: 200, message: "Invalid email", data: null });
+		const isUserExist = await getUserByEmail(email);
 
-	const isPhoneAllowed = phonePattern.test(phone);
-	if (!isPhoneAllowed) return res.status(200).json({ code: 200, message: "Invalid phone number", data: null });
+		if (!["male", "female"].includes(gender)) throwError("Unacceptable gender value", 400);
 
-	const isPasswordAllowed = passwordPattern.test(password);
-	if (!isPasswordAllowed) return res.status(200).json({ code: 200, message: "Password is too weak", data: null });
+		if (isUserExist) throwError("Email already registered", 409);
 
-	next();
+		next();
+	} catch (error) {
+		next(error);
+	}
 };
 
 /**
@@ -49,42 +54,39 @@ const validateRegister = async (req, res, next) => {
  * @param { import("express").NextFunction } next
  */
 const validateLogin = async (req, res, next) => {
-	const { email, password } = req.body;
-	const isPayloadEmpty = Object.keys(req.body).length === 0;
+	try {
+		const { email, password } = req.body;
 
-	const ADMIN_EMAIL = process.env.ADMIN_EMAIL;
-	const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
+		const isPayloadEmpty = Object.keys(req.body).length === 0;
 
-	if (!email || !password || isPayloadEmpty)
-		return res.status(400).json({ code: 400, message: "Please make sure all fields are filled in", data: null });
+		if (!email || !password || isPayloadEmpty) throwError("Please make sure all fields are filled in", 400);
 
-	if (
-		(email === ADMIN_EMAIL && password !== ADMIN_PASSWORD) ||
-		(email !== ADMIN_EMAIL && password === ADMIN_PASSWORD)
-	)
-		return res.status(200).json({ code: 200, message: "Invalid email or password", data: null });
+		const ADMIN_EMAIL = process.env.ADMIN_EMAIL;
+		const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
 
-	if (email === ADMIN_EMAIL && password === ADMIN_PASSWORD) {
+		if (
+			(email === ADMIN_EMAIL && password !== ADMIN_PASSWORD) ||
+			(email !== ADMIN_EMAIL && password === ADMIN_PASSWORD)
+		)
+			throwError("Invalid admin credentials", 200);
+
+		if (email === ADMIN_EMAIL && password === ADMIN_PASSWORD) return next();
+
+		const isEmailValid = email && emailPattern.test(email);
+
+		const user = isEmailValid ? await getUserByEmail(email) : null;
+		if (!user) throwError("User not found", 200);
+
+		const isPasswordMatch = user && (await bcrypt.compare(password, user.password));
+
+		if (!isEmailValid || !user || !isPasswordMatch) throwError("Invalid email or password", 200);
+
+		if (!user.is_verified) throwError("Cannot login, please verify your account first", 200);
+
 		next();
-		return;
+	} catch (error) {
+		next(error);
 	}
-
-	const isEmailAllowed = emailPattern.test(email);
-	if (!isEmailAllowed) return res.status(200).json({ code: 200, message: "Invalid email", data: null });
-
-	const user = await getUserByEmail(email);
-
-	if (!user) return res.status(200).json({ code: 200, message: "User not found", data: null });
-
-	const isPasswordMatch = await bcrypt.compare(password, user.password);
-	if (!isPasswordMatch) return res.status(200).json({ code: 200, message: "Invalid password", data: null });
-
-	if (!user.is_verified)
-		return res
-			.status(200)
-			.json({ code: 200, message: "Cannot login, please verify your account first", data: null });
-
-	next();
 };
 
 /**
@@ -94,20 +96,180 @@ const validateLogin = async (req, res, next) => {
  * @param { import("express").NextFunction } next
  */
 const validateVerification = async (req, res, next) => {
-	const verificationToken = req.query.token;
+	try {
+		const verificationToken = req.query.token;
 
-	if (!verificationToken)
-		return res.status(400).json({ code: 400, message: "Verification token is required", data: null });
+		if (!verificationToken) throwError("Verification token is required", 400);
 
-	const user = await getUserByVerifToken(verificationToken);
+		const user = await getUserByVerifToken(verificationToken);
 
-	if (!user) return res.status(200).json({ code: 200, message: "Token not recognized", data: null })
+		if (!user) throwError("Token not recognized", 200);
 
-	if (user.is_verified) return res.status(400).json({ code: 400, message: "User already verified", data: null });
-	
-	req.params.id = user.user_id
+		if (user.is_verified) throwError("User already verified", 200);
 
-	next();
+		req.params.id = user.user_id;
+
+		next();
+	} catch (error) {
+		next(error);
+	}
 };
 
-module.exports = { validateLogin, validateRegister, validateVerification };
+/**
+ *
+ * @param { import("express").Request } req
+ * @param { import("express").Response } res
+ * @param { import("express").NextFunction } next
+ */
+const validateRoleAccess = async (req, res, next) => {
+	try {
+		const { id: targetId } = req.params;
+		const { userId, role } = req.user;
+
+		const reqMethod = req.method;
+		const urlPath = req.path;
+
+		// Allow admin to access any resource
+		if (role === "admin") return next();
+
+		if (urlPath.includes("/upload")) return next();
+
+		if (role === "user") {
+			if (reqMethod !== "GET" && (urlPath.includes("course") || userId !== targetId))
+				throwError("You are not allowed to perform this action", 403);
+
+			if (userId !== targetId && reqMethod === "GET" && !urlPath.includes("course"))
+				throwError("You are not allowed to access this resource", 403);
+		}
+
+		next();
+	} catch (error) {
+		next(error);
+	}
+};
+
+/**
+ *
+ * @param { import("express").Request } req
+ * @param { import("express").Response } res
+ * @param { import("express").NextFunction } next
+ */
+const validateDataBeforeProceed = async (req, res, next) => {
+	try {
+		const reqMethod = req.method;
+		const data = req.body || {};
+
+		const {
+			name,
+			tagline,
+			description,
+			price,
+			is_discount,
+			discounted_price,
+			thumbnail_img_url,
+			categories,
+			modules
+		} = data;
+
+		const pattern = /^[1-9]\d*$/;
+
+		if (!["GET", "DELETE"].includes(reqMethod) && Object.keys(data).length === 0)
+			throwError("Cannot proceed with empty data", 400);
+
+		if (reqMethod === "PATCH" || reqMethod === "GET" || reqMethod === "DELETE") {
+			const id = +req.params.id;
+
+			if (!pattern.test(id)) throwError("Course ID must be a positive integer number", 400);
+		}
+
+		if (is_discount && !discounted_price) throwError("Please specify the discounted price", 400);
+
+		const isInvalidPrice = price < 0 || price % 1 !== 0 || !pattern.test(price);
+		const isInvalidDiscountedPrice =
+			is_discount && (discounted_price < 0 || discounted_price % 1 !== 0 || !pattern.test(discounted_price));
+
+		if (price && (isInvalidPrice || isInvalidDiscountedPrice)) throwError("Invalid price value", 400);
+
+		if (!is_discount && discounted_price && discounted_price > 0)
+			throwError("Discounted price is not applicable", 400);
+
+		if (is_discount && discounted_price >= price)
+			throwError("Discounted price cannot be equal or exceed the original price", 422);
+
+		if (reqMethod === "POST") {
+			if (!name || !tagline || !description || !price || !thumbnail_img_url || !categories || !modules)
+				throwError("Please make sure all fields are filled in", 400);
+
+			const isNameTaken = await getCourseByName(name);
+
+			if (isNameTaken) throwError("Course name already taken", 409);
+		}
+
+		if (reqMethod === "PATCH") {
+			const payload = req.body;
+			const { mode } = req.query;
+
+			if (is_discount && !discounted_price) throwError("Please specify the discounted price", 400);
+
+			if (!mode || mode === "default") {
+				if (!categories || categories.length === 0)
+					throwError("Failed to overwrite. No category provided", 400);
+			}
+
+			if (mode && !["default", "strict"].includes(mode))
+				throwError("Unknown mode. Mode must be either 'default' or 'strict'", 400);
+
+			if (mode === "strict") {
+				if (!categories || categories.length === 0) throwError("Failed to append. No category provided", 400);
+			}
+		}
+
+		next();
+	} catch (error) {
+		next(error);
+	}
+};
+
+/**
+ *
+ * @param { import("express").Request } req
+ * @param { import("express").Response } res
+ * @param { import("express").NextFunction } next
+ */
+const validateUserDataBeforeUpdate = async (req, res, next) => {
+	try {
+		const { role } = req.user;
+		const { id: targetId } = req.params;
+		const dataToUpdate = { ...req.body };
+		const uploadedImage = req.file;
+
+		if (Object.keys(dataToUpdate).length === 0) throwError("Cannot proceed with empty data", 400);
+
+		const user = await accessUserDataByRole(role, targetId);
+		if (!user) throwError("User not found", 200);
+
+		const isEmailAlreadyUsed = dataToUpdate.email && (await getUserByEmail(dataToUpdate.email));
+		if (isEmailAlreadyUsed) throwError("Email is already in use", 409);
+
+		if (uploadedImage && user.avatar_url.includes("res.cloudinary.com")) {
+			const publicId = user.avatar_url.split("/").pop().split(".")[0];
+			await deleteFromCloudinary(publicId);
+		}
+
+		dataToUpdate.avatar_url = uploadedImage.path;
+		req.dataToUpdate = dataToUpdate;
+
+		next();
+	} catch (error) {
+		next(error);
+	}
+};
+
+module.exports = {
+	validateLogin,
+	validateRegister,
+	validateVerification,
+	validateRoleAccess,
+	validateDataBeforeProceed,
+	validateUserDataBeforeUpdate
+};
