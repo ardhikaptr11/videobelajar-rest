0998e463d31074cf7d2036fe21831e03af311062
Ardhika <ardhikaptr11@gmail.com>
2025-05-25 11:38:09 +0700

    feat: add global error handler middleware to server setup

--- src/utils/server.js
+++ src/utils/server.js
@@ -2,17 +2,19 @@ const express = require("express");
 const cors = require("cors");
 
 const routes = require("../routes");
+const { globalErrorHandler } = require("../middleware/error.middleware");
 
 const createServer = () => {
-    const app = express();
+	const app = express();
 
 	app.use(express.urlencoded({ extended: true }));
 	app.use(express.json());
 	app.use(cors());
 
 	app.use(routes);
+	app.use(globalErrorHandler);
 
-    return app;
-}
+	return app;
+};
 
 module.exports = createServer;
