1be6b1b60d181781fc83cb02946a9f4fbdda6eef
Ardhika <ardhikaptr11@gmail.com>
2025-05-25 11:36:28 +0700

    feat: refactor email service to use centralized transporter and streamline email sending

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -1,29 +1,34 @@
-const nodemailer = require("nodemailer");
+const transporter = require("../config/nodemailer");
 
-const createTransport = () => {
-	const config =
-		process.env.NODE_ENV === "development"
-			? {
-					host: "smtp.ethereal.email",
-					port: 587,
-					secure: false,
-					auth: {
-						user: process.env.ETHEREAL_USERNAME,
-						pass: process.env.ETHEREAL_PASSWORD
-					}
-			  }
-			: {
-					host: "smtp.sendgrid.net",
-					port: 587,
-					secure: false,
-					auth: {
-						user: process.env.SMTP_USERNAME,
-						pass: process.env.SMTP_PASSWORD
-					}
-			  };
-	const transporter = nodemailer.createTransport(config);
+const { generateVerificationEmailContent } = require("../utils/emailContent");
 
-	return transporter;
+const sendEmail = async (to, subject, content) => {
+	const html = content;
+
+	try {
+		const mailOptions = {
+			from: process.env.SMTP_SENDER,
+			to,
+			subject,
+			html
+		};
+
+		const info = await transporter.sendMail(mailOptions);
+		console.log("✅ Email has been sent successfully:", info.messageId);
+		return info;
+	} catch (error) {
+		console.error("❌ Failed to send email:", error);
+	}
 };
 
-module.exports = createTransport;
+const sendVerificationEmail = async (user, token) => {
+	const url = process.env.VERCEL_URL || "http://localhost:8765";
+	const verificationURL = `${url}/api/v2/auth/verify-email?token=${token}`;
+
+	const subject = "[Required] - Verify your Email";
+	const content = generateVerificationEmailContent({ name: user.full_name, verificationURL });
+
+	return await sendEmail(user.email, subject, content);
+};
+
+module.exports = { sendVerificationEmail };
