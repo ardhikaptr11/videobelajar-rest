55b6ad7caa475b394c49355b40780d19f7013779
Ardhika <ardhikaptr11@gmail.com>
2025-05-25 11:28:40 +0700

    feat: update user and user_course tables to use UUID for user_id and enhance schema structure

--- src/database/migrations/20250427080456_create_users_table.js
+++ src/database/migrations/20250427080456_create_users_table.js
@@ -4,18 +4,23 @@
  */
 exports.up = (knex) => {
 	return knex.schema.createTable("users", (table) => {
-		table.increments("user_id").primary();
+		table.uuid("user_id").defaultTo(knex.fn.uuid()).primary();
+
 		table.string("full_name", 100).notNullable();
 		table.string("email").notNullable().unique();
-		table.string("phone", 20).notNullable();
 		table
 			.enu("gender", ["male", "female"], {
 				useNative: true,
 				enumName: "gender_enum"
 			})
 			.notNullable(),
-			table.text("password").notNullable(),
-			table.timestamps({ useTimestamps: true, defaultToNow: true });
+		table.string("phone", 20).notNullable();
+		table.text("password").notNullable(), 
+		table.string("avatar_url").nullable(),
+		table.string("verif_token", 8).notNullable();
+		table.bool("is_verified").defaultTo(false);
+
+		table.timestamps({ useTimestamps: true, defaultToNow: true });
 	});
 };
 
