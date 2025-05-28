51693a63fb81194fde6140af2a14e696353f06ea
Ardhika <ardhikaptr11@gmail.com>
2025-05-29 02:07:47 +0700

    fix: update swagger documentation setup to use correct file path and version endpoint

--- src/utils/swagger.js
+++ src/utils/swagger.js
@@ -1,14 +1,19 @@
 const express = require("express");
-const router = express.Router();
 const swaggerUi = require("swagger-ui-express");
-const YAML = require("yamljs");
+const fs = require("fs");
+const YAML = require("js-yaml");
 const path = require("path");
-const swaggerDocument = YAML.load(path.join(__dirname, "../../docs.yaml"));
+
+const router = express.Router();
+
+const filePath = path.join(path.join(__dirname, "../..", "docs/docs.yaml"));
+const fileContent = fs.readFileSync(filePath, "utf-8");
+const swaggerDocument = YAML.load(fileContent);
 
 const swaggerDocs = (app) => {
 	const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 
-	app.use("/docs/api/v1", swaggerUi.serve, swaggerUi.setup(swaggerDocument, { customCssUrl: CSS_URL }));
+	app.use("/docs/api/v2", swaggerUi.serve, swaggerUi.setup(swaggerDocument, { customCssUrl: CSS_URL }));
 };
 
 module.exports = swaggerDocs;
